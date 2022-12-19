0ff6ce5a3addcfa9de117fe9637805bbceb439e6
Vladislav Boichenko <me@brazy.one>
2022-12-19 15:59:00 +0300

    Fix infinite loading

--- components/GetNewMoviesPage.tsx
+++ components/GetNewMoviesPage.tsx
@@ -1,12 +1,27 @@
+import { Dispatch, SetStateAction, useEffect } from 'react';
 import useSWR from 'swr';
 
-export default function GetNewMoviesPage({ page }) {
+export default function GetNewMoviesPage({
+  page,
+  inView,
+  setPage,
+}: {
+  page: number;
+  inView: boolean;
+  setPage: Dispatch<SetStateAction<number>>;
+}) {
   const fetcher = (url: RequestInfo | URL) =>
     fetch(url).then((res) => res.json());
   const { data, error, isLoading } = useSWR(
     `/api/movies?page=${page}`,
     fetcher
   );
+
+  useEffect(() => {
+    if (inView && !isLoading && !error) {
+      setPage(page + 1);
+    }
+  });
 
   if (error) return <div>Failed to load</div>;
   if (isLoading) return <div>Loading...</div>;
--- components/MoviesPage.tsx
+++ components/MoviesPage.tsx
@@ -1,23 +1,23 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 import GetNewMoviesPage from './GetNewMoviesPage';
 
 export default function MoviesPage() {
   const [page, setPage] = useState(1);
-  const [ref, inView, entry] = useInView({
-    /* Optional options */
+  const [ref, inView] = useInView({
     threshold: 1,
-  });
-
-  useEffect(() => {
-    if (inView) {
-      setPage(page + 1);
-    }
   });
 
   const pages = [];
   for (let i = 0; i < page; i++) {
-    pages.push(<GetNewMoviesPage page={i + 1} key={i} />);
+    pages.push(
+      <GetNewMoviesPage
+        key={i}
+        page={i + 1}
+        inView={inView}
+        setPage={setPage}
+      />
+    );
   }
 
   return (
