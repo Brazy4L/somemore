12657d7a16c1053d221e8adc0abedfe8bc838d95
Vladislav Boichenko <me@brazy.one>
2022-12-22 21:31:22 +0300

    Add mobile menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,10 +4,10 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <nav className={styles.container}>
-      <div>
+      <div className={styles.logo}>
         <Link href="/">SOMEMORE</Link>
       </div>
-      <ul>
+      <ul className={styles.links}>
         <li>
           <Link href="/movies">Movies</Link>
         </li>
@@ -18,6 +18,27 @@ export default function Header() {
           <Link href="/about">About</Link>
         </li>
       </ul>
+      <input type="checkbox" id={styles.menu} />
+      <label htmlFor={styles.menu}>
+        <div className={styles.burger}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </label>
+      <div className={styles.menulinkscontainer}>
+        <ul className={styles.menulinks}>
+          <li>
+            <Link href="/movies">Movies</Link>
+          </li>
+          <li>
+            <Link href="/tvshows">TV Shows</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
