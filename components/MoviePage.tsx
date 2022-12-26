5eb47ddf41e637eca78b92b238dbc4139082cdc1
Vladislav Boichenko <me@brazy.one>
2022-12-26 21:00:52 +0300

    Fill movie page

--- components/MoviePage.tsx
+++ components/MoviePage.tsx
@@ -1,3 +1,4 @@
+import styles from '../styles/Movie.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -15,15 +16,56 @@ export default function MoviePage() {
   if (error) return <div>Failed to load</div>;
   if (isLoading) return <div>Loading...</div>;
 
+  const getDate = (date: Date) =>
+    new Date(date).toLocaleString('en-us', {
+      month: 'short',
+      day: 'numeric',
+      year: 'numeric',
+    });
+
   return (
-    <div key={data.id}>
-      <div>{data.title}</div>
-      <Image
-        width={780}
-        height={1170}
-        src={`https://image.tmdb.org/t/p/w780${data.poster_path}`}
-        alt=""
-      />
+    <div className={styles.container} key={data.id}>
+      <div className={styles.bgcontainer}>
+        <Image
+          className={styles.bg}
+          width={1280}
+          height={720}
+          src={`https://image.tmdb.org/t/p/w1280${data.backdrop_path}`}
+          alt=""
+        />
+      </div>
+      <div className={styles.details}>
+        <Image
+          className={styles.poster}
+          width={780}
+          height={1170}
+          src={`https://image.tmdb.org/t/p/w780${data.poster_path}`}
+          alt=""
+        />
+        <div>
+          {data.genres.map((el: { id: number; name: string }) => (
+            <div key={el.id}>{el.name}</div>
+          ))}
+        </div>
+        <div>
+          <div>{data.title}</div>
+          <div>{getDate(data.release_date)}</div>
+        </div>
+        <div>{data.overview}</div>
+        <div>
+          <Link
+            className={styles.link}
+            href={`https://www.imdb.com/title/${data.imdb_id}/`}
+          >
+            IMDb
+          </Link>
+        </div>
+        <div>
+          {data.keywords.keywords.map((el: { id: number; name: string }) => (
+            <div key={el.id}>{el.name}</div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
