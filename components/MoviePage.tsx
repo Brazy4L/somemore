1d26fd80be01f76bc3550d4cbb6b678623a7db7f
Vladislav Boichenko <me@brazy.one>
2023-01-04 22:03:13 +0300

    Add trailer

--- components/MoviePage.tsx
+++ components/MoviePage.tsx
@@ -23,69 +23,81 @@ export default function MoviePage() {
       year: 'numeric',
     });
 
+  const trailer = data.videos.results.find((el: { type: string }) => {
+    return el.type === 'Trailer';
+  });
+
   return (
     <>
       <Head>
         <title>{data.title} | SOMEMORE</title>
       </Head>
-      <div className="mx-auto box-content max-w-[1280px] text-slate-50">
-        <div className="-z-20 grid grid-cols-1 grid-rows-1 min-[540px]:absolute">
+      <div className="mx-auto box-content grid max-w-[1280px] grid-cols-[3fr,8fr] gap-4 pb-8 text-slate-50 min-[540px]:bg-[rgba(4,14,22,0.4)]">
+        <div className="-z-20 col-span-2 col-start-1 row-span-3 row-start-1">
           <Image
-            className="-z-20 col-start-1 row-start-1 min-[540px]:brightness-[0.5]"
+            className="-z-20 min-[540px]:brightness-[0.5]"
             width={1280}
             height={720}
             src={`https://image.tmdb.org/t/p/w1280${data.backdrop_path}`}
             alt=""
           />
-          <div className="col-start-1 row-start-1 self-end p-2 text-xl font-bold [text-shadow:_0px_1px_6px_rgb(0_0_0_/_100%)] min-[540px]:hidden min-[540px]:text-2xl">
-            {data.title}
-          </div>
         </div>
-        <div className="mt-2 grid gap-4 px-2 min-[540px]:mt-0 min-[540px]:bg-[rgba(4,14,22,0.4)] min-[540px]:px-8 min-[540px]:pt-2">
-          <Image
-            className="hidden w-1/3 rounded-lg border-[3px] border-gray-700 min-[540px]:block"
-            width={780}
-            height={1170}
-            src={`https://image.tmdb.org/t/p/w780${data.poster_path}`}
-            alt=""
-          />
-          <div className="hidden text-xl font-bold [text-shadow:_0px_1px_6px_rgb(0_0_0_/_100%)] min-[540px]:block min-[540px]:text-2xl">
-            {data.title}
+        <Image
+          className="min-[950px]:placeholder-shown:-4 col-start-1 row-start-1 hidden self-center min-[540px]:block"
+          width={780}
+          height={1170}
+          src={`https://image.tmdb.org/t/p/w780${data.poster_path}`}
+          alt=""
+        />
+        <div className="col-span-2 col-start-1 row-start-3 px-2 py-2 text-xl font-bold [text-shadow:_0px_1px_6px_rgb(0_0_0_/_100%)] min-[540px]:row-start-2 min-[540px]:py-0 min-[540px]:text-2xl min-[1300px]:text-4xl">
+          {data.title}
+        </div>
+        <div className="col-span-2 col-start-1 flex flex-wrap justify-between gap-1 px-2 min-[540px]:row-start-3">
+          <div className="text-slate-300 min-[540px]:[text-shadow:_0px_1px_6px_rgb(0_0_0_/_100%)]">
+            Release Date: {getDate(data.release_date)}
           </div>
-          <div className="flex flex-wrap justify-between gap-1">
-            <div className="text-slate-300 [text-shadow:_0px_1px_6px_rgb(0_0_0_/_100%)]">
-              Release Date: {getDate(data.release_date)}
-            </div>
-            <Link
-              className="font-bold"
-              href={`https://www.imdb.com/title/${data.imdb_id}/`}
-              target="_blank"
-              rel="noopener"
+          <Link
+            className="font-bold min-[540px]:[text-shadow:_0px_1px_6px_rgb(0_0_0_/_100%)]"
+            href={`https://www.imdb.com/title/${data.imdb_id}/`}
+            target="_blank"
+            rel="noopener"
+          >
+            🔗 IMDb
+          </Link>
+        </div>
+        <div className="col-span-2 flex flex-wrap justify-center gap-2 px-2 min-[540px]:row-start-4">
+          {data.genres.map((el: { id: number; name: string }) => (
+            <div
+              className="cursor-pointer rounded-full bg-gray-700 p-2 transition-colors hover:bg-gray-600"
+              key={el.id}
             >
-              🔗 IMDb
-            </Link>
-          </div>
-          <div className="flex flex-wrap justify-evenly gap-2">
-            {data.genres.map((el: { id: number; name: string }) => (
-              <div
-                className="cursor-pointer rounded-full bg-gray-700 p-2 transition-colors hover:bg-gray-600"
-                key={el.id}
-              >
-                {el.name}
-              </div>
-            ))}
-          </div>
-          <div>{data.overview}</div>
-          <div className="flex flex-wrap justify-evenly gap-2">
-            {data.keywords.keywords.map((el: { id: number; name: string }) => (
-              <div
-                className="cursor-pointer rounded-full bg-gray-700 p-2 transition-colors hover:bg-gray-600"
-                key={el.id}
-              >
-                {el.name}
-              </div>
-            ))}
-          </div>
+              {el.name}
+            </div>
+          ))}
+        </div>
+        <div className="col-span-2 px-2">{data.overview}</div>
+        <div className="col-span-2 flex flex-wrap justify-evenly gap-2 px-2 min-[540px]:row-start-6">
+          {data.keywords.keywords.map((el: { id: number; name: string }) => (
+            <div
+              className="cursor-pointer rounded-full bg-gray-700 p-2 transition-colors hover:bg-gray-600"
+              key={el.id}
+            >
+              {el.name}
+            </div>
+          ))}
+        </div>
+        <div className="col-span-2 aspect-video self-center min-[540px]:col-span-1 min-[540px]:col-start-2 min-[540px]:row-start-1">
+          {trailer && (
+            <iframe
+              className="h-full w-full"
+              width="560"
+              height="315"
+              src={`https://www.youtube-nocookie.com/embed/${trailer.key}`}
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              title="YouTube video player"
+            />
+          )}
         </div>
       </div>
     </>
