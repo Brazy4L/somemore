1b9855cae48b3705b3f01aeb61500de0551e8f94
Vladislav Boichenko <me@brazy.one>
2023-01-16 19:08:41 +0300

    Add reactplayer

--- components/Trailer.tsx
+++ components/Trailer.tsx
@@ -1,17 +1,15 @@
+import ReactPlayer from 'react-player/youtube';
+
 export default function Trailer(props: { trailerKey: string }) {
-  if (props.trailerKey === undefined) return <div>undefined</div>;
+  if (props.trailerKey === undefined) return <div></div>;
   return (
-    <>
-      <div>{props.trailerKey}</div>
-      <iframe
-        className="h-full w-full"
-        width="560"
-        height="315"
-        src={`https://www.youtube-nocookie.com/embed/${props.trailerKey}`}
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        title="YouTube video player"
-      />
-    </>
+    <ReactPlayer
+      light={true}
+      width={`100%`}
+      height={`100%`}
+      playing={true}
+      controls={true}
+      url={`https://www.youtube.com/watch?v=${props.trailerKey}`}
+    />
   );
 }
